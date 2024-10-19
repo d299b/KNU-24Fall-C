456f89763cc2847416c6f8b331fd5a9472aa9426
d299b <yun29836@gmail.com>
2024-10-20 00:49:52 +0900

    과제2

---
+++ day08/day08-1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main() {
+
+	char inpsen[100];
+	char tmp;
+
+	printf("문자열을 입력하세요(영어) : "); 
+	scanf_s("%s", &inpsen, 100);
+
+	int n = 0;
+	int len = strlen(inpsen) - 1;
+
+	while (len >= strlen(inpsen)/2) {
+		tmp = *(inpsen + len);
+		*(inpsen + len) = *(inpsen + n);
+		*(inpsen + n) = tmp;
+		n++;
+		len--;
+	}
+
+	n = 0;
+	len = strlen(inpsen) - 1;
+	while(len >= 0) {
+		printf("%c", *(inpsen + n));
+		len--;
+		n++;
+	}
+
+	return 0;
+}
---
+++ day08/day08-2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 10
+
+void fillRandom(int array[SIZE][SIZE]);
+void printArray(int array[SIZE][SIZE]);
+void movePointer(void* array);
+
+
+int main() {
+	int array[SIZE][SIZE];
+	
+	fillRandom(array);
+	printArray(array);
+	movePointer(array);
+
+	return 0;
+}
+
+void fillRandom(int array[SIZE][SIZE]) {
+	int rand_num = 0;
+	srand(time(NULL));
+
+	for (int i = 0; i < 10; i++) {
+		for (int r = 0; r < 10; r++) {
+		rand_num = rand() % 20 + 1;
+		array[i][r] = rand_num;
+		}
+	}
+	return array;
+}
+
+
+void printArray(int array[SIZE][SIZE]) {
+	for (int i = 0; i < 10; i++) {
+		for (int r = 0; r < 10; r++) {
+			printf("%2d ", array[i][r]);
+		}
+		printf("\n");
+	}
+	return array;
+}
+
+void movePointer(void* array) {
+	
+	//메모리 주소만 가져온다.
+	//배열이 무슨 형태인지 모름.
+	//형 변환
+
+	// *array == *&array[][] == array[][]
+
+	int i = 0;
+	int r = 0;
+	int count = 0;
+	int arrpit;
+
+	while (1){
+		arrpit = *(int*)array;
+		printf("현재 위치 : (%d, %d), 배열의 값 : %d\n", i, r, arrpit);
+
+		count = count + arrpit;
+		if (count >= 100) {
+			break;
+		}
+
+		i = count / 10;
+		r = count % 10;
+
+		array = (void*)((int*)array + arrpit);
+	}
+	printf("더 이상 이동할 수 없습니다.");
+	printf("종료 위치 : (%d, %d), 배열의 값 : %d", i, r, arrpit);
+}
